575fbe4500499578f57e85960850e56a3ac100ee
Amonel Leais <manoelaraujo24@gmail.com>
2024-02-09 18:53:45 -0300

    If correct display a screen

--- src/pages/ClassicMode.jsx
+++ src/pages/ClassicMode.jsx
@@ -14,6 +14,7 @@ export default function ClassicMode() {
     const [score, setScore] = useState(0);
     const [suggestions, setSuggestions] = useState([]);
     const [alreadyInserted, setAlreadyInserted] = useState([]);
+    const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
 
     const inputRef = useRef(null);
 
@@ -25,11 +26,7 @@ export default function ClassicMode() {
         setInputText("");
         setSuggestions([]);
         if (remainingAttempts === 1) {
-            setRemainingAttempts(6);
-            setPixelSize(17);
-            setScore(0);
-            getRandomAnime();
-            setAlreadyInserted([]);
+            resetGame();
         } else {
             getRandomAnime();
             setPixelSize(17);
@@ -48,21 +45,12 @@ export default function ClassicMode() {
         );
         if (isInputCorrect) {
             if (inputText.trim().toLowerCase() === animeTitle?.toLowerCase()) {
-                setPixelSize(17);
-                setRemainingAttempts(6);
-                setAlreadyInserted();
-                setScore(score + 1);
-                getRandomAnime();
-                setAlreadyInserted([]);
+                setAnswerCorrect();
             } else if (remainingAttempts - 1 === 0) {
-                setPixelSize(17);
-                setRemainingAttempts(6);
-                getRandomAnime();
-                setScore(0);
-                setAlreadyInserted([]);
+                resetGame();
             } else {
                 setRemainingAttempts(prevAttempts => prevAttempts - 1);
-                setPixelSize(pixelSize-3);
+                setPixelSize(pixelSize - 3);
                 setAlreadyInserted(prevNames => [inputText, ...prevNames]);
             }
         }
@@ -75,6 +63,24 @@ export default function ClassicMode() {
         const randomTitle = randomAnime.titles.find(title => title.type === "Default").title;
         setAnimeTitle(randomTitle);
         setAnimeCover(randomAnime.images.jpg.large_image_url);
+        setPixelSize(17);
+        setIsAnswerCorrect(false); // Reset answer correctness state
+    };
+
+    const resetGame = () => {
+        setPixelSize(17);
+        setRemainingAttempts(6);
+        setScore(0);
+        getRandomAnime();
+        setAlreadyInserted([]);
+    };
+
+    const setAnswerCorrect = () => {
+        setIsAnswerCorrect(true);
+        setPixelSize(0);
+        setRemainingAttempts(6);
+        setAlreadyInserted([]);
+        setScore(score + 1);
     };
 
     const handleInputChange = (e) => {
@@ -122,49 +128,66 @@ export default function ClassicMode() {
                     src={animeCover}
                 />}
             </div>
-            <div className="search__container" ref={inputRef}>
-                <div className="input__autocomplete">
-                    <input
-                        type="text"
-                        placeholder="Buscar..."
-                        value={inputText}
-                        onChange={handleInputChange}
-                    />
-                    <div className="autocomplete__list">
-                        {suggestions.length > 0 && (
-                            <ul>
-                                {suggestions.map((suggestion, index) => (
-                                    <li key={index} onClick={() => handleSuggestionClick(suggestion.title)}>
-                                        {suggestion.title}
-                                    </li>
-                                ))}
-                            </ul>
-                        )}
-                    </div>                   
-                </div>
-                <div className="btn__container">
-                    <button type="button" className="btn__pass" onClick={dontKnow}>NÃO SEI</button>
-                    <button type="button" className="btn__check" onClick={checkAnswer}>ADIVINHAR 推測</button>
-                </div>
+            <div className="if__correct">
+                {isAnswerCorrect && 
+                    <h1 className="anime_title">{animeTitle}</h1>
+                }
+                {isAnswerCorrect && 
+                    <button className="next_button" onClick={getRandomAnime}>Próximo</button>
+                }
+
             </div>
-            <div className="info__container">
-                <div className="effort__container">
-                    {[...Array(remainingAttempts)].map((_, index) => (
-                        <div key={index} className="effort__box"></div>
+            {!isAnswerCorrect &&
+                <div className="search__container" ref={inputRef}>
+                    <div className="input__autocomplete">
+                        <input
+                            type="text"
+                            placeholder="Buscar..."
+                            value={inputText}
+                            onChange={handleInputChange}
+                        />
+                        <div className="autocomplete__list">
+                            {suggestions.length > 0 && (
+                                <ul>
+                                    {suggestions.map((suggestion, index) => (
+                                        <li key={index} onClick={() => handleSuggestionClick(suggestion.title)}>
+                                            {suggestion.title}
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                        </div>                   
+                    </div>
+                    <div className="btn__container">
+                        <button type="button" className="btn__pass" onClick={dontKnow}>NÃO SEI</button>
+                        <button type="button" className="btn__check" onClick={checkAnswer}>ADIVINHAR 推測</button>
+                    </div>
+                </div>
+            }
+            {!isAnswerCorrect &&
+                <div className="info__container">
+                    <div className="effort__container">
+                        {[...Array(remainingAttempts)].map((_, index) => (
+                            <div key={index} className="effort__box"></div>
+                        ))}
+                    </div>
+                    <div className="score__container">
+                        <p className="score__display">{`Pontuação: ${score}`}</p>
+                    </div>
+                </div>
+            }
+            {!isAnswerCorrect &&
+                <div className="effort__text">
+                    <h5>{`${remainingAttempts} tentativas restantes`}</h5>
+                </div>
+            }
+            {!isAnswerCorrect &&
+                <div className="inserted__container">
+                    {alreadyInserted.map((name, index) => (
+                        <div key={index} className="inserted__name">{name}</div>
                     ))}
                 </div>
-                <div className="score__container">
-                    <p className="score__display">{`Pontuação: ${score}`}</p>
-                </div>
-            </div>
-            <div className="effort__text">
-                <h5>{`${remainingAttempts} tentativas restantes`}</h5>
-            </div>
-            <div className="inserted__container">
-                {alreadyInserted.map((name, index) => (
-                    <div key={index} className="inserted__name">{name}</div>
-                ))}
-            </div>
+            }
             <Footer />
         </div>
     )
